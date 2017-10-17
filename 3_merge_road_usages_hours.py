70ea693e0f1dfa412077d8adea1b206b13376520
ArktinenSieni <matti.raty@futurice.com>
2017-10-17 14:59:00 +0300

    updated default file-names, re-ran data-generation

--- 3_merge_road_usages_hours.py
+++ 3_merge_road_usages_hours.py
@@ -90,8 +90,8 @@ if __name__ == '__main__':
     import sys
     pd.options.mode.chained_assignment = None  # default='warn'
 
-    load_path = "data/3_road_usages.csv"
-    save_path = "data/4_road_usages.csv"
+    load_path = "data/2_road_usages.csv"
+    save_path = "data/3_road_usages.csv"
 
     try:
         load_path = sys.argv[1]
