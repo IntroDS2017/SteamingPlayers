0e6e5e881d7b8112a9bae53abaa05dc8c0dec459
ArktinenSieni <matti.raty@futurice.com>
2017-10-17 13:36:14 +0300

    dropped unclear road names

--- 2_modify_road_usages_names.py
+++ 2_modify_road_usages_names.py
@@ -1,29 +1,36 @@
 import pandas as pd
 
+
 def modify_street(street):
     for i in range(len(old_names)):
         if street == old_names[i]:
             return new_names[i]
     return street
 
+
+def drop_rows_by_road_names(road_names, df):
+    return df[-df['nimi'].isin(road_names)]
+
+
 if __name__ == '__main__':
 
     load_path = "data/2_road_usages.csv"
-    save_path = "data/3_road_usages.csv"
+    save_path = "data/42_road_usages.csv"
 
     df_road_usages = pd.read_csv(load_path)
 
-    # TODO: remove rows with names "VALIMOT.+KIRKKOTIE", "MALMIK. + OJAM.", "PORVOONVÄYLÄ 1" and "PORVOONVÄYLÄ 2"
-    # TODO: should we just drop rows with "VALIMOT.+KIRKKOTIE" and "MALMIK. + OJAM." instead? Those streets seem to be far from each other in map.
-    old_names = ["V.PORVOONTIE", "VALIMOT.+KIRKKOTIE", "PORVOONVÄYLÄ 1", "PORVOONVÄYLÄ 2", "UUSI PORVOONTTIE", "KEHÄ III", "MALMIK.  + OJAM."]
-    new_names = ["VANHA PORVOONTIE", "VALIMOTIE", "PORVOONVÄYLÄ", "PORVOONVÄYLÄ", "UUSI PORVOONTIE", "KEHÄ 3", "MALMINKATU"]
+    old_names = ["V.PORVOONTIE", "UUSI PORVOONTTIE", "KEHÄ III"]
+    new_names = ["VANHA PORVOONTIE", "UUSI PORVOONTIE", "KEHÄ 3"]
 
     print("\nOriginal unique road names count: " + str(len(df_road_usages.nimi.unique())))
     print("Original unique road names:")
     print(df_road_usages.nimi.unique())
 
-    # Changes PORVOONVÄYLÄ 1 & 2 to PORVOONVÄYLÄ. PORVOONVÄYLÄ also itself already existed to begin with.
+    # Removing unclear road-names.
+    roads_to_drop = ["VALIMOT.+KIRKKOTIE", "MALMIK.  + OJAM.", "PORVOONVÄYLÄ 1", "PORVOONVÄYLÄ 2"]
+    df_road_usages = drop_rows_by_road_names(roads_to_drop, df_road_usages)
 
+    # Renaming road names to match the other data-set
     df_road_usages['nimi'] = df_road_usages['nimi'].map(modify_street)
 
     df_road_usages.to_csv(save_path, index = False)
