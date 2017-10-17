1baab134bfba35977fbc6d18d794b9c27a6fee99
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2017-10-17 16:32:04 +0300

    generateData.sh fix

--- generateData.sh
+++ generateData.sh
@@ -7,7 +7,7 @@ echo "(1/4) Renaming streets, dropping uncertain road-addresses..."
 python3 2_modify_road_usages_names.py
 
 echo "(2/4) Merging car-count by hour..."
-python3 3_merge_read_usages_hours.py
+python3 3_merge_road_usages_hours.py
 
 echo "(3/4) Formatting accident-data address-names to match road-usage-data..."
 python3 4_change_accidents_katuosoite.py
