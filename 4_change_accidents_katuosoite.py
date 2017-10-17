e3aaf86aa2dee5f8686c38e68b7db41118bf3ba6
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2017-10-17 14:45:05 +0300

    solution for mapping accidents Katuosoite names to match with names of road_usages

--- 4_change_accidents_katuosoite.py
+++ 4_change_accidents_katuosoite.py
@@ -1,27 +1,48 @@
 import pandas as pd
 
-# Changes address into one of the unique road names in road_usages, or makes it empty if no comparison can be made.
+def modify_specific_street(address):
+    new_addr = address
+
+    if 'LAHDEN MOOTTORITIE'.lower() in address.lower():
+        new_addr = "LAHDENVÄYLÄ"
+    elif 'KEHÄ III'.lower() in address.lower(): # must be before 'KEHÄ I' check!
+        new_addr = "KEHÄ 3"
+    elif 'KEHÄ I'.lower() in address.lower():
+        new_addr = 'KEHÄ 1'
+
+    if address != new_addr:
+        specific_changes_log.write("Address " + address + " changed into " + new_addr + "\n")
+    return new_addr
+
 def modify_katuosoite(address):
+    stripped_address = address.strip()
+    combined_address = "".join(stripped_address.lower().split(" ")) # changes KEHÄ 1 --> kehä1
+
     for road_name in road_usages_names:
-        if road_name in address:
-            if (road_name != address.strip()):
-                print("Address " + address + " changed into " + road_name)
+        combined_road_name = "".join(road_name.lower().split(" ")) # changes KEHÄ 1 --> kehä1
+
+        if combined_road_name in combined_address:
+
+            if (road_name != stripped_address):
+                change_log.write("Address " + stripped_address + " changed into " + road_name + "\n")
+            else:
+                non_changed_rows_count[0] += 1
+
             return road_name
-    return "NOT_FOUND: " + address # TODO: hack to maintain the address information if still needed further on.
+
+    not_found_log.write(address + " not found\n")
+    return "NOT_FOUND: " + address # TODO: maintain the address information if still needed further on
 
 
-def main():
-    print(df_accidents.Katuosoite.unique())
-    print(len(df_accidents.Katuosoite.unique()))
-
-    df_accidents['Katuosoite'] = df_accidents['Katuosoite'].map(modify_katuosoite)
-    print(df_accidents.Katuosoite.unique())
-    print(len(df_accidents.Katuosoite.unique()))
-
-    df_accidents.to_csv("data/5_accidents.csv", index = False)
+def main(accidents_save_path):
+    df_accidents['Katuosoite'] = df_accidents['Katuosoite'].map(modify_specific_street).map(modify_katuosoite)
+    df_accidents.to_csv(accidents_save_path, index = False)
 
 
 if __name__ == '__main__':
+    specific_changes_log = open('logs/4_katusoite_specific_changes.log', 'w')
+    change_log = open('logs/4_katuosoite_changes.log', 'w')
+    not_found_log = open('logs/4_katuosoite_not_founds.log', 'w')
 
     accidents_load_path = "data/1_accidents.csv"
     road_usages_load_path = "data/4_road_usages.csv"
@@ -32,4 +53,16 @@ if __name__ == '__main__':
     accidents_save_path = "data/5_accidents.csv"
 
     road_usages_names = df_road_usages.nimi.unique()
-    main()
+
+    non_changed_rows_count = [0] # reference global int
+    main(accidents_save_path)
+
+    specific_changes_log.close()
+    change_log.close()
+    not_found_log.close()
+
+    print("Non changed rows count: " + str(non_changed_rows_count[0]))
+
+    # 1193 non changed rows, 2561 changed rows, 6327 not found rows.
+    # Total amount of rows that can be mapped together with road_usages: 1193 + 2561 = 3588
+    # Total amount of rows: 10081
