f3dd5b6308777c15be7ff2dfc3a93ab54569d89b
Mika Huttunen <mika.huttunen@Mika-Huttunen.local>
2017-10-20 10:37:06 +0300

    did some code refactoring#

--- 3_merge_road_usages_hours.py
+++ 3_merge_road_usages_hours.py
@@ -1,59 +1,5 @@
 import pandas as pd
 
-
-def flatten_list(to_flat):
-    """
-    Flattens 2D-list to 1d
-    :param to_flat: List to flatten
-    :return: 1D list
-    """
-    return [row for rows in to_flat for row in rows]
-
-
-def handle_grouped_by_aika(group):
-    """
-    Sums car-amounts of given rows. Also time is increased by one to match accident-data. It is intended that group has
-    the same 'aika'.
-    :param group: Car-amounts to sum.
-    :return: First row of the group, with summed 'autot'
-    """
-    rows = group[1]
-
-    cars = rows['autot'].sum()
-
-    result = rows.iloc[0].copy()
-    result['autot'] = cars
-    result['aika'] += 1
-
-    return result
-
-
-def handle_grouped_by_suunta(df_tuple):
-    """
-    Divides 'aika' column by 100, and groups rows by 'aika', and proceeds to pass this value to sum 'autot'.
-    :param df_tuple: grouped-by - groups-tuple.
-    :return: Two rows with summed 'auto':t, for each 'suunta'
-    """
-    groups = df_tuple[1]
-
-    groups['aika'] = groups['aika'].apply(lambda x: int(x / 100)) # normalize aika
-    grouped_by_aika = group_by_column(groups, 'aika')
-
-    return map(handle_grouped_by_aika, grouped_by_aika)
-
-
-def handle_grouped_by_piste(df_tuple):
-    """
-    Splits rows by 'suunta', direction, and proceeds to sum 'auto'-column, cars for each hour.
-    :param df_tuple: grouped-by - groups tuple. Should be already grouped by 'piste', measurement point
-    :return: flatten result of rows, which have summed 'auto', car-amount for each hour
-    """
-    grouped_by_suunta = group_by_column(df_tuple[1], 'suunta')
-    result = map(handle_grouped_by_suunta, grouped_by_suunta)
-
-    return flatten_list(result)
-
-
 # TODO: could be removed
 def group_by_column(df, c_name):
     """
@@ -66,16 +12,67 @@ def group_by_column(df, c_name):
     return df.groupby(c_name)
 
 
+def flatten_list(to_flat):
+    """
+    Flattens 2D-list to 1d
+    :param to_flat: List to flatten
+    :return: 1D list
+    """
+    return [row for rows in to_flat for row in rows]
+
+
+def sum_over_autot_of_rows_sharing_aika(grouped_roads):
+    """
+    Sums car-amounts of given rows. Also time is increased by one to match accident-data. It is intended that group has
+    the same 'aika'.
+    :param grouped_roads: Car-amounts to sum.
+    :return: First row of the group, with summed 'autot'
+    """
+    roads = grouped_roads[1]
+
+    cars = roads['autot'].sum()
+
+    result = roads.iloc[0].copy() # TODO: what does this do?
+    result['autot'] = cars
+    result['aika'] += 1
+
+    return result
+
+
+def handle_roads_by_id_and_suunta(grouped_roads):
+    """
+    Divides 'aika' column by 100, and groups rows by 'aika', and proceeds to pass this value to sum 'autot'.
+    :param piste_suunta_grouped: grouped-by - groups-tuple.
+    :return: Two rows with summed 'auto':t, for each 'suunta'
+    """
+    roads = grouped_roads[1]
+
+    roads['aika'] = roads['aika'].apply(lambda x: int(x / 100)) # set for example each 700, 715, 730, and 745 to 7, for summing over them
+
+    result = map(sum_over_autot_of_rows_sharing_aika, group_by_column(roads, 'aika'))
+    return result # TODO: add flattening here?
+
+
+def handle_roads_by_id(grouped_roads):
+    """
+    Splits rows by 'suunta', direction, and proceeds to sum 'auto'-column, cars for each hour.
+    :param piste_grouped: grouped-by - groups tuple. Should be already grouped by 'piste', measurement point
+    :return: flatten result of rows, which have summed 'auto', car-amount for each hour
+    """
+    roads = grouped_roads[1]
+    result = map(handle_roads_by_id_and_suunta, group_by_column(roads, 'suunta'))
+    return flatten_list(result)
+
+
 def main(road_usages_data_path):
     """
     Merges hours in 2_road_usages.csv
     :param road_usages_data_path: String, where file is loaded from.
     :return: New dataframe. If time-unit was between some hour, they were summed.
     """
-    road_usages = pd.read_csv(road_usages_data_path)
-    grouped_by_piste = group_by_column(road_usages, 'piste')
+    roads = pd.read_csv(road_usages_data_path)
 
-    result = map(handle_grouped_by_piste, grouped_by_piste)
+    result = map(handle_roads_by_id, group_by_column(roads, 'piste')) # piste = unique ID that stands for given road. Could use also 'nimi' here.
     flat_result = flatten_list(result)
 
     return pd.DataFrame(flat_result)
