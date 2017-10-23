554087023af7fa62012fb7787f381f9bff932823
EKettu <elina@Elinas-MacBook-Pro.local>
2017-10-23 15:17:25 +0300

    Some plots and a file

--- old_code_files/9_some_graphs.py
+++ old_code_files/9_some_graphs.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import seaborn as sns
 
+def calculate_ratio(accidents, traffic):
+    if traffic == 0:
+        return 0.0
+    return 1.0*accidents/traffic
+
 
 def main():
     """
@@ -8,23 +13,22 @@ def main():
     dataset. 
     """
 
-    df = pd.read_csv('data/9_combined.csv')
+    df = pd.read_csv('data/7_combined_left_join.csv')
 
-    df_accidents = df.loc[df['Onnettomuus'] == 1]
-
+    
     #Data on the accidents per year on the streets of Helsinki
-    accidents = df_accidents[['nimi', 'vuosi', 'Onnettomuus']].groupby(['nimi', 'vuosi']).count()['Onnettomuus']
+    accidents = df[['nimi', 'vuosi', 'Onnett_id']].groupby(['nimi', 'vuosi']).count()['Onnett_id']
 
     print(accidents)
 
     df_accidents = accidents.to_frame()
     df_accidents.reset_index(inplace=True)
-    df_accidents.columns = ['Katuosoite', 'Vuosi', 'Onnettomuuksia']
+    df_accidents.columns = ['Street', 'Year', 'Accidents']
     
     sns.set(font_scale = 2) #Increase the font size
     
-    accidentplot = sns.factorplot(x='Katuosoite', y='Onnettomuuksia', kind='bar', size=50, 
-               data=df_accidents, hue='Vuosi')
+    accidentplot = sns.factorplot(x='Street', y='Accidents', kind='bar', size=50, 
+               data=df_accidents, hue='Year')
     accidentplot.set_xticklabels(rotation=90) 
 
     accidentplot.savefig('data/figures/1_accidentplot.png')
@@ -34,15 +38,36 @@ def main():
    
     traffic_df = traffic.to_frame()
     traffic_df.reset_index(inplace=True)
-    traffic_df.columns = ['Katu', 'Vuosi', 'Autoja']
+    traffic_df.columns = ['Street', 'Year', 'Cars']
+    
+    #A rough estimate of all traffic on a street during one year
+    traffic_df['Autoja'] = traffic_df['Cars'].apply(lambda x: x*365)
 
     
-    trafficplot = sns.factorplot(x='Katu', y='Autoja', kind ='bar', size=50, 
-               data=traffic_df, hue='Vuosi')
+    trafficplot = sns.factorplot(x='Street', y='Cars', kind ='bar', size=50, 
+               data=traffic_df, hue='Year')
 
     trafficplot.set_xticklabels(rotation=90)
 
     trafficplot.savefig('data/figures/2_trafficplot.png')
+    
+    #A new combined dataset for calculating Accidents/Traffic ratio
+    df_combined = pd.merge(traffic_df, df, how = 'left', 
+                           left_on=['Street', 'Year'], 
+                           right_on=['Street', 'Year'])
+    
+    
+    df_combined['Accidents/Traffic']=df_combined.apply(lambda row: 
+                  calculate_ratio(row.Onnettomuuksia,row.Autoja), axis=1)
+        
+    
+    accident_ratioplot = sns.factorplot(x='Street', y='Accidents/Traffic', kind ='bar', size=50, 
+               data=df_combined, hue='Year')
+
+    accident_ratioplot.set_xticklabels(rotation=90)
+    
+    trafficplot.savefig('data/figures/3_accident_ratioplot.png')
+      
     
     
 
