7871b63653641a091e273c05715731323039919e
ArktinenSieni <matti.raty@futurice.com>
2017-10-23 16:51:48 +0300

    rebased with moreGraphs, added initial map

--- 8_mapping.py
+++ 8_mapping.py
@@ -20,18 +20,27 @@ def read_em():
 added = []
 
 
-def plot_em(gpd_dict):
-    base = gpd_dict['city'].plot(color='gray', edgecolor='white')
-    gpd_dict['moottori'].plot(ax=base, color='black')
-    gpd_dict['alu_kok'].plot(ax=base, color='cyan')
-    gpd_dict['kokooja'].plot(ax=base, color='orange')
-    gpd_dict['paa'].plot(ax=base, color='blue')
-    gpd_dict['asunto'].plot(ax=base, color='yellow')
+def plot_em(gpd_dict, show_names=False):
+    base = gpd_dict['city'].plot(color='gray', edgecolor='white', linewidth=1.0, zorder=0)
+    gpd_dict['moottori'].plot(ax=base, color='black', linewidth=1.0, markersize=1, zorder=1)
+    gpd_dict['alu_kok'].plot(ax=base, color='cyan', linewidth=0.5, markersize=0.5, zorder=1)
+    gpd_dict['kokooja'].plot(ax=base, color='orange', linewidth=0.9, markersize=0.9, zorder=1)
+    gpd_dict['paa'].plot(ax=base, color='blue', linewidth=0.6, markersize=0.6, zorder=1)
+    gpd_dict['asunto'].plot(ax=base, color='yellow', linewidth=0.2, markersize=0.2, zorder=1)
 
     points = gpd_dict['points']
-    point_ax = points.plot(ax=base, color='red', marker='*', zorder=2)
+    point_ax = points.plot(ax=base, color='red', marker='*', zorder=2, markersize=1.5)
 
-    gpd_dict['points'].apply(lambda x: annotate_point(x, point_ax), axis=1)
+    x1, x2, y1, y2 = plt.axis()
+
+    point_ax.set_xlim([x1, x2])
+    point_ax.set_ylim([6670000, 6687500])
+
+    point_ax.set_axis_off()
+
+    if show_names:
+        gpd_dict['points'].apply(lambda x: annotate_point(x, point_ax), axis=1)
+
     added.clear()
 
 
@@ -60,7 +69,10 @@ if __name__ == '__main__':
     gpd_dict['points'] = usage_df_to_gpd(usage_load_path)
 
     plot_em(gpd_dict)
+
+    plt.savefig('./data/figures/4_measurement_points.png', format='png', bbox_inches='tight', dpi=500)
     plt.show()
+
 
 
 
